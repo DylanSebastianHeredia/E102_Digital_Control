02d172a287e6fce4631feb8d3e2eb1738fbb5ab9
mrmorale17 <alesilva@g.hmc.edu>
2026-05-12 15:59:36 -0700

    final Arduino code for Part 5

--- parts_3_4_5/e102_digital_controller_part5.ino
+++ parts_3_4_5/e102_digital_controller_part5.ino
@@ -16,9 +16,9 @@ boolean switchVal = HIGH;    // declare initial switch pin state
 // Parameter Settings 
 int time = 0;  // initialize time 
 int uVal = 0;  // initialize control input 
-const float Kp = 2.35; 
-const float Ki = 0.86;
-const float ref = 2.5; //2.5V reference input 
+const float Kp = 2.34923; 
+const float Ki = 0.85505;
+float ref = 0; 
  
 // Initial Values for internal signals 
 float y=0; 
@@ -28,35 +28,35 @@ float u_prev=0;
 float e_prev=0;
 const float T=0.1; // Sample time
  
-void setup() {  
-  pinMode(switchPin, INPUT);  //set switch pin to input mode 
-  digitalWrite(switchPin,HIGH); //initialize to start with pull up voltage      
-  Serial.begin(9600);  
-} 
+void setup() {
+
+  pinMode(switchPin, INPUT);
+  digitalWrite(switchPin, HIGH);
+
+  pinMode(uPin, OUTPUT);
+  analogWrite(uPin, 0);
+
+  Serial.begin(9600);
+
+  delay(20000);
+}
  
-void loop() { 
-  //WAIT FOR SWITCH 
-   while(switchVal == HIGH)  // repeat this loop until switch is turned on 
-                            // (switchVal will go LOW when switch is turned on) 
-  {    
-    analogWrite(uPin,0);   
-    switchVal = digitalRead(switchPin);  // read switch state 
-  } 
-   
-   
- // READ CIRCUIT OUTPUT 
-  int sensorVal = analogRead(yPin); 
-      // convert to volts 
-      y=sensorVal*(5.0/1023.0); 
-      // calculate error 
-      e=ref-y; 
+void loop() {    
+    ref = 2.5;
+
+// READ CIRCUIT OUTPUT 
+    int sensorVal = analogRead(yPin); 
+    // convert to volts 
+    y=sensorVal*(5.0/1023.0); 
+    // calculate error 
+    e=ref-y; 
        
   // CONTROLLER 
      
      u = u_prev + (Kp + Ki*T)*e - Kp*e_prev;
-      
-     e_prev = e;
-     u_prev = u; 
+
+    e_prev = e;
+    u_prev = u; 
 
      // check that control signal is in range 
      if (u>5) 
@@ -67,9 +67,10 @@ void loop() {
      { 
      u=0; 
      } 
+
       
   // WRITE CIRCUIT INPUT  
-     uVal=u*(255/5); 
+     uVal = (int)(u * 255.0 / 5.0); 
      analogWrite(uPin,uVal); 
    
  // print the results to the serial monitor:  
